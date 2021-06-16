201d5a097a7562761d8dac8cb936c6346223ea30
Mihail Curmei <mcurmei@mail.ru>
2021-06-16 20:02:16 +0300

    Re-added permission settings

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,20 @@
 import GuestPostingSettings from './components/GuestPostingSettings';
+import {extend} from 'flarum/extend';  
+import PermissionGrid from 'flarum/components/PermissionGrid';
 
 app.initializers.add('block-cat/guest-posting', () => {
+  extend(PermissionGrid.prototype, 'startItems', (items) => {
+    items.replace(
+      'start',
+      {
+        icon: 'fas fa-edit',
+        label: app.translator.trans('core.admin.permissions.start_discussions_label'),
+        permission: 'startDiscussion',
+        allowGuest: true,
+      },
+      100
+    );
+  });
   app.extensionData.for('block-cat-guest-posting')
     .registerPage(GuestPostingSettings);
 });
